be0cfc50d4ca6f3db815cb168bcddda435a7fa99
paixaoariellll <paixaoariellll@gmail.com>
2023-01-20 16:46:16 -0300

    erro

--- sections/user/index.jsx
+++ sections/user/index.jsx
@@ -1,6 +1,6 @@
 import Hero from './Hero';
 import Form from './Form';
 import Confirm from './Confirm';
-import Login from './Login';
+import Login from './Login2';
 
 export { Hero, Form, Confirm, Login };
