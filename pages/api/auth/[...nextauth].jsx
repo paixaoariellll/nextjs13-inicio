dabb4e10d3092fd5ba469ef34b4efae21958f98d
paixaoariellll <paixaoariellll@gmail.com>
2023-01-20 17:30:17 -0300

    next

--- pages/api/auth/[...nextauth].jsx
+++ pages/api/auth/[...nextauth].jsx
@@ -10,20 +10,6 @@ export default NextAuth({
   session: {
     strategy: 'jwt',
   },
-  callbacks: {
-    async jwtToken({ token, user }) {
-      const newToken = { ...token };
-      if (user?._id) newToken._id = user._id;
-      if (user?.isAdmin) newToken.isAdmin = user.isAdmin;
-      return newToken;
-    },
-    async jwtSession({ session, token }) {
-      const newSession = { ...session };
-      if (token?._id) newSession._id = token._id;
-      if (token?.isAdmin) newSession.isAdmin = token.isAdmin;
-      return newSession;
-    },
-  },
   providers: [
     CredentialsProvider({
       async authorize(credentials, req) {
